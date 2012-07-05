8bbd29b76d529f640b08e0ff61e80b45934c564c
Lucas Serafim <lslucas@gmail.com>
2012-07-05 15:02:43 -0500

    favicon, bg e padding

--- admin/inc.header.php
+++ admin/inc.header.php
@@ -26,7 +26,7 @@
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-	<link rel="shortcut icon" href="<?=$rp?>../public/images/favicon.ico">
+	<link rel="shortcut icon" href="<?=$rp?>../public/images/favicon.png">
     <!-- STYLESHEETS -->
     <?php 
 	if (isset($include_css)) 
