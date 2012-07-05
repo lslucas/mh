fa18e67c439e14846aa44c25dcd6b2ae172971e8
Lucas Serafim <lslucas@gmail.com>
2012-07-05 11:39:50 -0500

    zend path

--- admin/_inc/global.php
+++ admin/_inc/global.php
@@ -83,7 +83,7 @@ if ($_SERVER['HTTP_HOST']=='localhost') {
  $base = $host.$path.'/';
 
  if( strpos($host, 'localhost')!=false ) ini_set('include_path', '.:/Users/lucasserafim/Sites/Zend/library.:/opt/local/lib/php');
- else ini_set('include_path', '.:/home/content/88/9481088/html/');
+ else ini_set('include_path', '.:/home/content/98/8229398/html');
 
 
 
