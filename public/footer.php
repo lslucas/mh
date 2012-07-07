c79bfcfec62dfe5c85d22c3378a4abae540e50d0
Lucas Serafim <lslucas@gmail.com>
2012-07-06 23:26:11 -0500

    maskedinput

--- public/footer.php
+++ public/footer.php
@@ -9,6 +9,7 @@
 		var ABSPATH = '<?=ABSPATH?>';
 	</script>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="<?=JS?>jquery.maskedinput-1.3.min.js"></script>
 	<script src="<?=$rph?>public/bootstrap/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?=JS?>application.js.php"></script>
 	<script type='text/javascript'>
--- public/js/application.js.php
+++ public/js/application.js.php
@@ -7,3 +7,49 @@
 	$rp = '../../admin/';
 	include_once '../../load.php';
 ?>
+	$(function() {
+
+		/*
+		 *aplica mascara
+		 */
+		if ($('input[name="cpf"], .cpf'))
+			$('input[name="cpf"], .cpf').mask('999.999.999-99');
+
+		if ($('input[name="cnpj"], .cnpj'))
+			$('input[name="cnpj"], .cnpj').mask('99.999.999/9999-99');
+
+		if ($('input[name="cep"], .cep'))
+			$('input[name="cep"], .cep').mask('99.999-999');
+
+		if ($('.phone'))
+			$('.phone').mask('(99) 9999-9999');
+
+		if ($('.date'))
+			$('.date').mask('99/99/9999');
+
+
+		$('input, textarea').keydown(function(){
+			if ($(this).attr('maxlength') && $(this).attr('maxlength')>0) {
+
+				var maxlen = $(this).attr('maxlength');
+				var actuallen = ($(this).val().length+1);
+				var resul = maxlen-actuallen;
+				var name = $(this).attr('name');
+				var txt = '';
+				
+				if (resul>1)
+					txt = ' Faltam '+resul+' caracteres!';
+				else if (resul==0 || resul==-1)
+					txt = ' Você chegou ao limite de '+maxlen+' caracteres!';
+				else
+					txt = ' Falta 1 caractere!';
+
+				if ($('span#'+name).length==0)
+					$('<br/><span class="small" id="'+name+'">'+txt+'</span>').insertAfter(this);
+				else
+					$('span#'+name).text(txt);
+
+			}
+		});
+
+	});
